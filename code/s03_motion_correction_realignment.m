872045c9cb808c8d81193fb3c1f4e230f93e4b9b
Dulce Travassos <158324644+dulcetravassos@users.noreply.github.com>
2026-02-15 15:28:26 +0000

    Creates JSON for each new file

--- code/s03_motion_correction_realignment.m
+++ code/s03_motion_correction_realignment.m
@@ -4,7 +4,9 @@
 %                                                                        %
 %   Corrects for head movement. Inputs are 'a...' files (Slice Timed).   %
 %   Outputs are 'ra...' files, plus 'mean...' and 'rp_... .txt'. Also    %
-%   saves an updated JSON file.                                          %
+%   saves an updated JSON file for all NIfTI outputs and creates a JSON  % 
+%   file for the 'rp_*.txt'. This script is adapted to deal with         %
+%   variable volumes.                                                    %
 %                                                                        %
 %   Author: Dulce Travassos                                              %
 %   Created: 13/02/2026                                                  %
@@ -26,10 +28,11 @@ deriv_dir = fullfile(base_dir,'derivatives','spm-preprocessing');
 
 % List of Subjects
 subjects = {
-    'sub-002', 'sub-003', 'sub-004', 'sub-006', 'sub-007', 'sub-008', ...
-    'sub-009', 'sub-011', 'sub-012', 'sub-013', 'sub-014', 'sub-015', ...
-    'sub-016', 'sub-017', 'sub-018', 'sub-019', 'sub-020', 'sub-021', ...
-    'sub-022', 'sub-023'
+    'sub-002', 
+    % 'sub-003', 'sub-004', 'sub-006', 'sub-007', 'sub-008', ...
+    % 'sub-009', 'sub-011', 'sub-012', 'sub-013', 'sub-014', 'sub-015', ...
+    % 'sub-016', 'sub-017', 'sub-018', 'sub-019', 'sub-020', 'sub-021', ...
+    % 'sub-022', 'sub-023'
 };
 
 % Volumes - Localizer Task
@@ -38,25 +41,25 @@ vol_localizer = 190;
 % Volumes - Main Task
 volumes_main = [
     206, 250, 246, 206, 224, 223, 222, 223; % sub-002
-    226, 223, 222, 223, 223, 223, 223, 223; % sub-003
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-004
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-006
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-007
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-008
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-009
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-011
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-012
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-013
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-014
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-015
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-016
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-017
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-018
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-019
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-020
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-021
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-022
-    223, 223, 223, 223, 223, 223, 223, 223; % sub-023
+    % 226, 223, 222, 223, 223, 223, 223, 223; % sub-003
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-004
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-006
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-007
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-008
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-009
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-011
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-012
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-013
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-014
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-015
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-016
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-017
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-018
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-019
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-020
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-021
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-022
+    % 223, 223, 223, 223, 223, 223, 223, 223; % sub-023
 ];
 
 %% Realignment/Motion Correction
@@ -167,6 +170,28 @@ for s = 1:length(subjects)
             end
 
         end
+
+        % SPM creates a mean image, that also needs a JSON!
+        if ~isempty(files_for_json)
+            mean_name = ['mean' files_for_json{1}];
+            mean_dest = fullfile(subj_func_dir,mean_name);
+            if exist(mean_dest,'file')
+                init_json_path = fullfile(subj_func_dir,replace(files_for_json{1},'.nii','.json')); % using the first func file JSON as template
+                out_json_path = replace(mean_dest,'.nii','.json');
+                create_derivative_json(init_json_path,out_json_path,'MeanImage');
+            end
+        end
+
+        % SPM creates a file 'rp_*.txt', that also needs a JSON...
+        for k = 1:length(files_for_json)
+            rp_name = ['rp_' replace(files_for_json{k},'.nii','.txt')];
+            rp_dest = fullfile(subj_func_dir,rp_name);
+            if exist(rp_dest,'file')
+                out_json_path = replace(rp_dest,'.txt','.json');
+                create_rp_json(out_json_path);
+            end
+        end
+
         fprintf('  > Done for %s!\n', subj);
     end
 end
@@ -175,7 +200,9 @@ fprintf('\nFinished!\n')
 
 %% Helper Functions
 
-function create_derivative_json(source_json_path,target_json_path)
+function create_derivative_json(source_json_path,target_json_path,type)
+
+if nargin<3; type = 'Functional'; end; % if missing type
 
 if exist(source_json_path,'file')
     content = fileread(source_json_path);
@@ -189,11 +216,31 @@ else
 end
 
 json_data.MotionCorrection = true;
+json_data.MotionCorrectionSoftware = 'SPM12';
 json_data.Sources = {source_json_path};
+
+if strcmp(type,'MeanImage')
+    json_data.Description = 'Mean EPI image generated by SPM Realignment';
+end
 
 % Save .json file
 fid = fopen(target_json_path,'w');
 if fid==-1; warning('Could not save JSON file.'); return; end
+fprintf(fid,'%s',jsonencode(json_data,'PrettyPrint',true));
+fclose(fid);
+end
+
+
+function create_rp_json(target_json_path)
+json_data = struct();
+json_data.Description = 'SPM Motion Estimation Parameters';
+json_data.Columns = {'x','y','z','pitch','roll','yaw'};
+json_data.Units = {'mm','mm','mm','rad','rad','rad'};
+json_data.MotionCorrectionSoftware = 'SPM12';
+
+% Save .json file
+fid = fopen(target_json_path,'w');
+if fid==-1; warning('Could not save RP JSON.'); return; end
 fprintf(fid,'%s',jsonencode(json_data,'PrettyPrint',true));
 fclose(fid);
 end
